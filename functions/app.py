1d2665cea182d903c1325de9b2063473892b025c
Addison <captainadd49@gmail.com>
2023-09-17 05:11:42 -0400

    addi changes

--- functions/app.py
+++ functions/app.py
@@ -1,19 +1,18 @@
 import datetime
 import json
 from flask import Flask, request, jsonify, send_from_directory
-
+import json
 from waitress import serve
 from flask_cors import CORS, cross_origin
 from firebase_functions import https_fn, db_fn, options
 from firebase_admin import db, initialize_app
 from firebase_admin import credentials
 from prompts.final import Janus
-
 DB_URL = "https://janus-4326f-default-rtdb.firebaseio.com"
 cred = credentials.Certificate("./functions/janus-4326f-62cc9d861e34.json")
 initialize_app(cred, {'databaseURL': DB_URL})
-
-
+import logging
+import sys
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 # app.config['CORS_HEADERS'] = 'Content-Type'
@@ -29,7 +28,8 @@ def day_entry():
     """Create a journal entry for a given day."""
 
     print("day entry")
-    uid = request.json["uid"]
+    print(request.json, "data input")
+    uid = request.json['data']["uid"]
     today = datetime.datetime.strftime("%Y-%m-%d")
 
     # entries = db.reference(f"days/{uid}").get()
@@ -43,7 +43,7 @@ def day_entry():
 
     jns = Janus(vision_list, goal_list, att_list)
 
-    evaluation = jns.evaluate(request.json["entry"])
+    evaluation = jns.evaluate(request.json['data']["entry"])
 
     for cat_str, cat_idx in enumerate(list(user_metrics.keys())):
         for i in user_metrics[cat_str].keys():
@@ -56,10 +56,15 @@ def day_entry():
     return jsonify(user_metrics)
 
 
-@app.route('/read-metrics/', methods=['GET', 'POST'])
-def read_metrics():
-    uid = request.json["uid"]
-    return jsonify(db.reference(f"metrics/{uid}").get())
+@app.route('/read-metrics/<uid>', methods=['GET'])
+# @cross_origin(supports_credentials=True)
+def read_metrics(uid):
+    # uid = request.json["uid"]
+    # uid = int(
+    print(f'{uid=}')
+    ret = db.reference(f"metrics/{uid}").get()
+    print("ret", ret)
+    return jsonify(ret)
 
 
 @app.route('/read-days/', methods=['GET', 'POST'])
@@ -69,4 +74,4 @@ def read_days():
 
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=2000, url_scheme='https', threads=8)
+    serve(app, host='0.0.0.0', port=5000, url_scheme='http', threads=8,debug=True)
